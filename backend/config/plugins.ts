a74ea207c7091da5d9b1d28f0186f2cebb008b02
Alena Lyalyushkina <pluto_16@mail.ru>
2024-11-25 17:15:09 +1000

    fix: update server.ts

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,8 +1,1 @@
-export default () => ({
-  'content-manager': {
-    config: {
-      // Измените базовый путь для Content Manager
-      apiPath: '/admin/content-manager', // Ваш новый путь
-    },
-  },
-});
+export default () => ({});
